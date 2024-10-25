59684a079e7fc525ce28bcd2ba6aaa1e11174790
Gustavo Trevisol <gustavotrevisol@MacBook-Air-de-Gustavo-4.local>
2024-10-25 15:40:38 -0300

    exclui app

--- app/ACMEVideos.java
+++ app/ACMEVideos.java
@@ -1,13 +1,62 @@
 package app;
 import dados.Acervo;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.PrintStream;
+import java.nio.charset.Charset;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class ACMEVideos {
+	private Scanner entrada = new Scanner(System.in);  // Atributo para entrada padrao (teclado)
+    private PrintStream saidaPadrao = System.out;   // Guarda a saida padrao - tela (console)
+    private final String nomeArquivoEntrada = "entrada.txt";  // Nome do arquivo de entrada de dados
+    private final String nomeArquivoSaida = "saida.txt";  // Nome do arquivo de saida de dados
 
-	private Acervo acervo;
+
+	public ACMEVideos(){
+		redirecionaEntrada();    // Redireciona Entrada para arquivos
+        redirecionaSaida();    // Redireciona Saida para arquivos
+	}
 
 	public void processar() {
-		App app = new App();
-		app.executar();
+		
 	}
+
+	private void redirecionaEntrada() {
+        try {
+            BufferedReader streamEntrada = new BufferedReader(new FileReader(nomeArquivoEntrada));
+            entrada = new Scanner(streamEntrada);   // Usa como entrada um arquivo
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
+        entrada.useLocale(Locale.ENGLISH);   // Ajusta para leitura para ponto decimal
+    }
+
+    // Redireciona Saida de dados para arquivos em vez da tela (terminal)
+    // Chame este metodo para redirecionar a escrita de dados para arquivos
+    private void redirecionaSaida() {
+        try {
+            PrintStream streamSaida = new PrintStream(new File(nomeArquivoSaida), Charset.forName("UTF-8"));
+            System.setOut(streamSaida);             // Usa como saida um arquivo
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        Locale.setDefault(Locale.ENGLISH);   // Ajusta para ponto decimal
+    }
+
+    // Restaura Entrada padrao para o teclado
+    // Chame este metodo para retornar a leitura de dados para o teclado
+    private void restauraEntrada() {
+        entrada = new Scanner(System.in);
+    }
+
+    // Restaura Saida padrao para a tela (terminal)
+    // Chame este metodo para retornar a escrita de dados para a tela
+    private void restauraSaida() {
+        System.setOut(saidaPadrao);
+    }
 
 }
